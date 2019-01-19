e2fbb32a1eebaa231e17cfc05ff9b908f34f1920
JanetM9 <chenlin@stanford.edu>
2019-01-19 00:31:55 -0800

    code

--- train.py
+++ train.py
@@ -1,13 +1,12 @@
 import os
 import argparse
 import pickle
-#from model_architecture import *
 from updated_model import *
 
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 device = torch.device('cuda')
-
 #device = torch.device('cpu')
+
 n_epochs = 300
 batch_size_train = 64
 batch_size_test = 64#1000
@@ -54,8 +53,6 @@ test_loader = torch.utils.data.DataLoader(
 
 
 net = Net(BasicBlockA, BasicBlockB,[4,4,1,1],[1,1,1,1],image_size=32, input_channel=3).to(device)
-#net = DimIncrease(3, 3, stride=1, kernel=3).to(device)
-#net = Net(BasicBlockA, BasicBlockB,[2,1,1,1], image_size=32,input_channel=3).to(device)
 #optimizer = optim.SGD(net.parameters(), weight_decay=1e-4, lr=learning_rate,
                       #momentum=momentum)
 optimizer = optim.Adam(net.parameters(), weight_decay=1e-4, lr=learning_rate)
@@ -63,7 +60,6 @@ optimizer = optim.Adam(net.parameters(), weight_decay=1e-4, lr=learning_rate)
 # Train the model
 net.train()
 stats = {}
-
 stats['train_loss'] = []
 stats['train_accuracy'] = []
 stats['val_loss'] = []
@@ -74,9 +70,6 @@ for _ in range(n_epochs):
 	if _  % 50 ==0 :
 		for g in optimizer.param_groups:
 			g['lr'] /= 1.5
-	#else:
-	#	for g in optimizer.param_groups:
-	#		g['lr'] -= 5e-7
 
 	print('Now processing epoch {}'.format(_))
 	train_loss = 0
@@ -86,21 +79,16 @@ for _ in range(n_epochs):
 	for batch_idx, (data, target) in enumerate(train_loader):
 		data, target = data.to(device), target.to(device)
 		output = net.forward(data)
-		#pdb.set_trace()
 		loss = F.nll_loss(output, target)
-
 		pred = output.data.max(1, keepdim=True)[1]
 		train_correct += pred.eq(target.data.view_as(pred)).sum()
 		total_train += target.shape[0]
-		#pdb.set_trace()
 		params = list(net.parameters())
 		train_loss += loss.item()
 		# Backward and optimize
 		optimizer.zero_grad()
 		loss.backward(retain_graph=True)
 		optimizer.step()
-		#print(net.conv2.bias.grad)
-	#pdb.set_trace()
 	train_loss = train_loss * batch_size_train/ len(train_loader.dataset)
 	stats['train_loss'].append(train_loss)
 	stats['train_accuracy'].append((100. * train_correct/total_train).cpu().numpy())
@@ -118,8 +106,6 @@ for _ in range(n_epochs):
 			pred = output.data.max(1, keepdim=True)[1]
 			correct += pred.eq(target.data.view_as(pred)).sum()
 		test_loss /= len(test_loader.dataset)
-		#test_losses.append(test_loss)
-		#pdb.set_trace()
 		stats['val_loss'].append(test_loss)
 		stats['val_accuracy'].append((100. * correct / len(test_loader.dataset)).cpu().numpy())
 		print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
