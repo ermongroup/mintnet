9777361fa86456df06bf3c2d1e20b1e281c69672
JanetM9 <chenlin@stanford.edu>
2019-04-04 22:40:34 -0700

    classification updated

--- train.py
+++ train.py
@@ -9,36 +9,31 @@ device = torch.device('cuda')
 
 n_epochs = 300
 batch_size_train = 64
-batch_size_test = 64  # 1000
-learning_rate = 1e-3  # 5e-7
+batch_size_test = 64  
+learning_rate = 1e-3 
 momentum = 0.9
-log_interval = 10
 
-# MNIST
-# train_loader = torch.utils.data.DataLoader(
-# torchvision.datasets.MNIST('./files/', train=True, download=True,
-#                            transform=torchvision.transforms.Compose([
-#                              torchvision.transforms.ToTensor(),
-#                              torchvision.transforms.Normalize(
-#                                (0.1307,), (0.3081,))
-#                            ])),
-# batch_size=batch_size_train, shuffle=True)
+train_loader = torch.utils.data.DataLoader(
+torchvision.datasets.MNIST('./files/', train=True, download=True,
+                            transform=torchvision.transforms.Compose([
+                              torchvision.transforms.ToTensor(),
+                            ])),
+batch_size=batch_size_train, shuffle=True)
 
-# test_loader = torch.utils.data.DataLoader(
-# torchvision.datasets.MNIST('./files/', train=False, download=True,
-#                            transform=torchvision.transforms.Compose([
-#                              torchvision.transforms.ToTensor(),
-#                              torchvision.transforms.Normalize(
-#                                (0.1307,), (0.3081,))
-#                            ])),
-# batch_size=batch_size_test, shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+torchvision.datasets.MNIST('./files/', train=False, download=True,
+                            transform=torchvision.transforms.Compose([
+                              torchvision.transforms.ToTensor(),
+                            ])),
+batch_size=batch_size_test, shuffle=True)
 
+net = Net(BasicBlockA, BasicBlockB, [2, 2, 1, 1], [2, 2, 1, 1], image_size=28, input_channel=1).to(device)
+
+'''
 train_loader = torch.utils.data.DataLoader(
     torchvision.datasets.CIFAR10('./dataset/', train=True, download=True,
                                  transform=torchvision.transforms.Compose([
-                                     torchvision.transforms.ToTensor(),
-                                     torchvision.transforms.Normalize(
-                                         (0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
+                                     torchvision.transforms.ToTensor(),                                     
                                  ])),
     batch_size=batch_size_train, shuffle=True)
 
@@ -46,24 +41,20 @@ test_loader = torch.utils.data.DataLoader(
     torchvision.datasets.CIFAR10('./dataset/', train=False, download=True,
                                  transform=torchvision.transforms.Compose([
                                      torchvision.transforms.ToTensor(),
-                                     torchvision.transforms.Normalize(
-                                         (0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
                                  ])),
     batch_size=batch_size_test, shuffle=True)
 
-net = Net(BasicBlockA, BasicBlockB, [4, 4, 1, 1], [1, 1, 1, 1], image_size=32, input_channel=3).to(device)
-# optimizer = optim.SGD(net.parameters(), weight_decay=1e-4, lr=learning_rate,
-# momentum=momentum)
+net = Net(BasicBlockA, BasicBlockB, [4, 4, 1, 1], [32, 32, 1, 1], image_size=32, input_channel=3).to(device)
+'''
+
+
 optimizer = optim.Adam(net.parameters(), weight_decay=1e-4, lr=learning_rate)
 
-# Train the model
-net.train()
 stats = {}
 stats['train_loss'] = []
 stats['train_accuracy'] = []
 stats['val_loss'] = []
 stats['val_accuracy'] = []
-# train_loss = 0
 lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=1 / 1.5)
 
 for _ in range(n_epochs):
@@ -76,15 +67,17 @@ for _ in range(n_epochs):
     train_correct = 0
 
     net.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
+    for batch_idx, (data, target) in enumerate(train_loader):       
+        if data.shape[0] != batch_size_train: continue
+     
         data, target = data.to(device), target.to(device)
         output = net(data)
         loss = F.nll_loss(output, target)
         pred = output.data.max(1, keepdim=True)[1]
         train_correct += pred.eq(target.data.view_as(pred)).sum()
         total_train += target.shape[0]
-        params = list(net.parameters())
         train_loss += loss.item()
+        
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward(retain_graph=True)
@@ -97,20 +90,25 @@ for _ in range(n_epochs):
     # Test
     net.eval()
     test_loss = 0
+    total_test = 0
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
+            if data.shape[0] != batch_size_train: continue            
+            total_test += data.shape[0]            
             data, target = data.to(device), target.to(device)
             output = net(data)
+            
             test_loss += F.nll_loss(output, target, size_average=False).item()
             pred = output.data.max(1, keepdim=True)[1]
             correct += pred.eq(target.data.view_as(pred)).sum()
-        test_loss /= len(test_loader.dataset)
+            
+        test_loss /= total_test
         stats['val_loss'].append(test_loss)
         stats['val_accuracy'].append((100. * correct / len(test_loader.dataset)).cpu().numpy())
         print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, len(test_loader.dataset),
-            100. * correct / len(test_loader.dataset)))
-
+            test_loss, correct, total_test,
+            100. * correct / total_test))
+    
     with open('./train_stats', 'wb') as f:
         pickle.dump(stats, f, protocol=pickle.HIGHEST_PROTOCOL)
--- updated_model.py
+++ updated_model.py
@@ -14,117 +14,101 @@ os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 device = torch.device('cuda')
 
 
-# device = torch.device('cpu')
-
-def leaky_relu_derivative(x, slope):
-    if x > 0:
-        return 1
-    return slope
-
-
-# DO NOT FORGET ACTNORM!!!
+#DO NOT FORGET ACTNORM!!!
 class BasicBlockA(nn.Module):
-    # Input_dim should be 1(grey scale image) or 3(RGB image), or other dimension if use SpaceToDepth
+#Input_dim should be 1(grey scale image) or 3(RGB image), or other dimension if use SpaceToDepth
     def __init__(self, latent_dim, stride=1, input_dim=3, kernel=3):
         super(BasicBlockA, self).__init__()
         self.input_dim = input_dim
-        self.latent_dim = latent_dim
+        self.latent_dim = latent_dim               
         self.kernel = kernel
         self.weight_list1 = nn.ParameterList()
-        self.center_list = nn.ParameterList()
+        self.center_list1 = nn.ParameterList()
         self.bias_list1 = nn.ParameterList()
-
+        self.res = nn.Parameter(torch.ones(1))
+        
         for i in range(latent_dim):
             weight = torch.Tensor(input_dim, input_dim, kernel, kernel)
-            bias = torch.Tensor(input_dim)
+            bias = torch.Tensor(input_dim) 
             nn.init.xavier_normal_(weight)
             nn.init.normal_(bias)
             self.weight_list1.append(nn.Parameter(weight))
             self.bias_list1.append(nn.Parameter(bias))
-            center = np.random.randn(input_dim, input_dim, kernel, kernel)
-            self.center_list.append(nn.Parameter(torch.Tensor(center)))
-
+            center = torch.Tensor(np.random.randn(input_dim, input_dim, kernel, kernel))
+            self.center_list1.append(nn.Parameter(center))
+            
+        self.center_list2 = nn.ParameterList()
         self.weight_list2 = nn.ParameterList()
         self.bias_list2 = nn.ParameterList()
-        for i in range(latent_dim ** 2):
+        for i in range(latent_dim):
+            center = torch.Tensor(np.random.randn(input_dim, input_dim, kernel, kernel))
             weight = torch.Tensor(input_dim, input_dim, kernel, kernel)
             nn.init.xavier_normal_(weight)
             bias = torch.Tensor(input_dim)
             nn.init.normal_(bias)
+            self.center_list2.append(nn.Parameter(center))
             self.weight_list2.append(nn.Parameter(weight))
             self.bias_list2.append(nn.Parameter(bias))
 
         # Define masks
-        kernel_mid_y, kernel_mid_x = kernel // 2, kernel // 2
+        kernel_mid_y, kernel_mid_x = kernel//2, kernel//2
         # zero in the middle(technically not middle, depending on channels), one elsewhere
         # used to mask out the diagonal element
         self.mask0 = np.ones((input_dim, input_dim, kernel, kernel), dtype=np.float32)
-
+  
         # 1 in the middle, zero elsewhere, used for center mask to zero out the non-diagonal element
         self.mask1 = np.zeros((input_dim, input_dim, kernel, kernel), dtype=np.float32)
 
         # Mask out the element above diagonal
-        self.mask = np.ones((input_dim, input_dim, kernel, kernel), dtype=np.float32)
+        self.mask = np.ones((input_dim, input_dim, kernel, kernel), dtype=np.float32)        
 
-        # For RGB ONLY:i=0:Red channel;i=1:Green channel;i=2:Blue channel
+        #For RGB ONLY:i=0:Red channel;i=1:Green channel;i=2:Blue channel 
         for i in range(input_dim):
-            self.mask0[i, i, kernel_mid_y, kernel_mid_x] = 0.0
-            self.mask1[i, i, kernel_mid_y, kernel_mid_x] = 1.0
-
-            self.mask[i, :, kernel_mid_y + 1:, :] = 0.0
+            self.mask0[i,i,kernel_mid_y,kernel_mid_x] = 0.0
+            self.mask1[i,i,kernel_mid_y,kernel_mid_x] = 1.0
+            self.mask[i,:, kernel_mid_y+1:, :] = 0.0
             # For the current and previous color channels, including the current color
-            self.mask[i, :i + 1, kernel_mid_y, kernel_mid_x + 1:] = 0.0
-
+            self.mask[i,:i+1, kernel_mid_y, kernel_mid_x+1:] = 0.0
             # For the latter color channels, not including the current color
-            # self.mask[i,i+1:,kernel_mid_y+1:, :] = 0.0
-            self.mask[i, i + 1:, kernel_mid_y, kernel_mid_x:] = 0.0
-
+            self.mask[i, i+1:, kernel_mid_y, kernel_mid_x:] = 0.0
         self.mask0 = torch.Tensor(self.mask0).to(device)
         self.mask1 = torch.Tensor(self.mask1).to(device)
         self.mask = torch.Tensor(self.mask).to(device)
 
-        # double check latent_output(feature) is correct?
-        # self.bn1 = nn.BatchNorm2d(input_dim)
 
     def forward(self, x):
-        residual = x
-        latent1 = []
+        residual = x        
+        latent1 = []  
         for i in range(self.latent_dim):
-            latent_output = F.conv2d(x,
-                                     (self.weight_list1[i] * self.mask0 + self.center_list[i] * self.mask1) * self.mask,
-                                     bias=self.bias_list1[i], padding=1)
-            # latent_output = self.bn1(latent_output)
-            latent_output = F.leaky_relu(latent_output, negative_slope=0.1)
+            latent_output = F.conv2d(x, (self.weight_list1[i]*self.mask0 + torch.nn.functional.softplus(self.center_list1[i])*self.mask1)*self.mask, bias=self.bias_list1[i], padding=1)
+            latent_output = F.elu(latent_output, alpha=1)
             latent1.append(latent_output)
-
+        
         latent2 = []
         for i in range(self.latent_dim):
-            for j in range(self.latent_dim):
-                latent_output = F.conv2d(latent1[j],
-                                         (self.weight_list2[i * self.latent_dim + j] * self.mask0 + self.center_list[
-                                             i] * self.mask1) * self.mask,
-                                         bias=self.bias_list2[i * self.latent_dim + j], padding=1)
-                latent2.append(latent_output)
-
-        # check whether it's the correct sum
+            latent_output = F.conv2d(latent1[i],\
+            (self.weight_list2[i]*self.mask0 + torch.nn.functional.softplus(self.center_list2[i]) *self.mask1)*self.mask, padding=1)
+            latent2.append(latent_output)
+     
         output = torch.stack(latent2, dim=0)
-        output = output.sum(dim=0) / len(latent2)
-        # output = self.bn1(output)
-        output += residual
+        output = output.sum(dim=0)/len(latent2)
+        mask_res = (self.res>0).float().to(device)
+        output = output + self.res * mask_res * residual 
         return output
 
 
 class BasicBlockB(nn.Module):
-    # input_dim should be 1(grey scale image) or 3(RGB image)
+#input_dim should be 1(grey scale image) or 3(RGB image)
     def __init__(self, latent_dim, stride=1, input_dim=3, kernel=3):
         super(BasicBlockB, self).__init__()
         self.input_dim = input_dim
-        self.latent_dim = latent_dim
+        self.latent_dim = latent_dim               
         self.kernel = kernel
         self.weight_list1 = nn.ParameterList()
-        self.center_list = nn.ParameterList()
+        self.center_list1 = nn.ParameterList()
         self.bias_list1 = nn.ParameterList()
-
+        self.res = nn.Parameter(torch.ones(1))
+        
         for i in range(latent_dim):
             weight = torch.Tensor(input_dim, input_dim, kernel, kernel)
             bias = torch.Tensor(input_dim)
@@ -132,71 +116,69 @@ class BasicBlockB(nn.Module):
             nn.init.normal_(bias)
             self.weight_list1.append(nn.Parameter(weight))
             self.bias_list1.append(nn.Parameter(bias))
-            center = np.random.randn(input_dim, input_dim, kernel, kernel)
-            self.center_list.append(nn.Parameter(torch.Tensor(center)))
+            center = torch.Tensor(np.random.randn(input_dim, input_dim, kernel, kernel))
+            self.center_list1.append(nn.Parameter(center))
 
+        self.center_list2 = nn.ParameterList()
         self.weight_list2 = nn.ParameterList()
         self.bias_list2 = nn.ParameterList()
-        for i in range(latent_dim ** 2):
+        for i in range(latent_dim):
+            center = torch.Tensor(np.random.randn(input_dim, input_dim, kernel, kernel))
             weight = torch.Tensor(input_dim, input_dim, kernel, kernel)
             nn.init.xavier_normal_(weight)
             bias = torch.Tensor(input_dim)
             nn.init.normal_(bias)
+            self.center_list2.append(nn.Parameter(center))
             self.weight_list2.append(nn.Parameter(weight))
             self.bias_list2.append(nn.Parameter(bias))
 
         # Define masks
-        kernel_mid_y, kernel_mid_x = kernel // 2, kernel // 2
+        kernel_mid_y, kernel_mid_x = kernel//2, kernel//2
         # zero in the middle(technically not middle, depending on channels), one elsewhere
         # used to mask out the diagonal element
         self.mask0 = np.ones((input_dim, input_dim, kernel, kernel), dtype=np.float32)
-
+  
         # 1 in the middle, zero elsewhere, used for center mask to zero out the non-diagonal element
         self.mask1 = np.zeros((input_dim, input_dim, kernel, kernel), dtype=np.float32)
 
         # Mask out the element above diagonal
-        self.mask = np.ones((input_dim, input_dim, kernel, kernel), dtype=np.float32)
+        self.mask = np.ones((input_dim, input_dim, kernel, kernel), dtype=np.float32)        
 
-        # i=0:Red channel;i=1:Green channel;i=2:Blue channel
+        #i=0:Red channel;i=1:Green channel;i=2:Blue channel
         for i in range(input_dim):
-            self.mask0[i, i, kernel_mid_y, kernel_mid_x] = 0.0
-            self.mask1[i, i, kernel_mid_y, kernel_mid_x] = 1.0
-            self.mask[i, :, :kernel_mid_y, :] = 0.0
+            self.mask0[i,i,kernel_mid_y,kernel_mid_x] = 0.0
+            self.mask1[i,i,kernel_mid_y,kernel_mid_x] = 1.0
+            self.mask[i,:, :kernel_mid_y, :] = 0.0
             # For the current and latter color channels, including the current color
-            self.mask[i, i:, kernel_mid_y, :kernel_mid_x] = 0.0
+            self.mask[i,i:, kernel_mid_y, :kernel_mid_x] = 0.0
             # For the previous color channels, not including the current color
-            self.mask[i, :i, kernel_mid_y, :kernel_mid_x + 1] = 0.0
-
+            self.mask[i,:i, kernel_mid_y, :kernel_mid_x+1] = 0.0
         self.mask0 = torch.Tensor(self.mask0).to(device)
         self.mask1 = torch.Tensor(self.mask1).to(device)
         self.mask = torch.Tensor(self.mask).to(device)
-        # double check latent_output(feature) is correct?
-        # self.bn1 = nn.BatchNorm2d(input_dim)
+
 
     def forward(self, x):
-        residual = x
-        latent1 = []
+        residual = x        
+        latent1 = []  
+        
         for i in range(self.latent_dim):
-            latent_output = F.conv2d(x,
-                                     (self.weight_list1[i] * self.mask0 + self.center_list[i] * self.mask1) * self.mask,
-                                     bias=self.bias_list1[i], padding=1)
-            # latent_output = self.bn1(latent_output)
-            latent_output = F.leaky_relu(latent_output, negative_slope=0.1)
+            latent_output = F.conv2d(x, (self.weight_list1[i]*self.mask0 + torch.nn.functional.softplus(self.center_list1[i]) * self.mask1)*self.mask, bias=self.bias_list1[i], padding=1)
+            latent_output = F.elu(latent_output, alpha=1)   
             latent1.append(latent_output)
 
         latent2 = []
+        # remove for loops. Whether can do everything using one convolution. torch.einsum()
         for i in range(self.latent_dim):
-            for j in range(self.latent_dim):
-                latent_output = F.conv2d(latent1[j], \
-                                         (self.weight_list2[i * self.latent_dim + j] * self.mask0 + self.center_list[
-                                             j] * self.mask1) * self.mask,
-                                         bias=self.bias_list2[i * self.latent_dim + j], padding=1)
-                latent2.append(latent_output)
-        # check whether it's the correct sum
+            latent_output = F.conv2d(latent1[i],\
+            (self.weight_list2[i]*self.mask0 + torch.nn.functional.softplus(self.center_list2[i]) *self.mask1)*self.mask, bias=self.bias_list2[i], padding=1)
+            latent2.append(latent_output)
+         
         output = torch.stack(latent2, dim=0)
-        output = output.sum(dim=0) / len(latent2)
-        # output = self.bn1(output)
-        output += residual
+        output = output.sum(dim=0)/len(latent2)
+
+        mask_res = (self.res>0).float().to(device)
+        output = output + self.res * mask_res * residual
         return output
 
 
@@ -251,10 +233,10 @@ class Net(nn.Module):
         self.increase_dim = SpaceToDepth(4)
         self.layer1 = self._make_layer(layer_size[0], blockA, blockB, latent_size[0], channel)
         # ADD MORE LAYERS
-        channel *= 4 * 4
+        #channel *= 4 * 4
         self.layer2 = self._make_layer(layer_size[1], blockA, blockB, latent_size[1], channel)
-        # self.layer3 = self._make_layer(layer_size[2], blockA, blockB, latent_size[2],channel)
-        # self.layer4 = self._make_layer(layer_size[3], blockA, blockB, latent_size[3],channel)
+        self.layer3 = self._make_layer(layer_size[2], blockA, blockB, latent_size[2],channel)
+        self.layer4 = self._make_layer(layer_size[3], blockA, blockB, latent_size[3],channel)
         self.fc = nn.Linear(input_channel * image_size * image_size, num_classes)
 
     def _make_layer(self, block_num, blockA, blockB, latent_dim, input_dim, stride=1):
@@ -266,9 +248,11 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = self.layer1(x)
-        x = self.increase_dim(x)
+        #x = self.increase_dim(x)
         x = self.layer2(x)
-
+        x = self.layer3(x)
+        x = self.layer4(x)
+        
         x = x.view(x.shape[0], -1)
         x = self.fc(x)
         return F.log_softmax(x, dim=1)
